86212602f4c3fb3007106a6dfb563522e3199e2e
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-12-30 12:23:44 -0600

    Switch to 24.11 nixpkgs (#24)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Achieve nirvana through automatically following all flake inputs.";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
   outputs = {
     self,
